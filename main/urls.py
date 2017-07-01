9da110de23316751264f847b9a48ddf5156dfbe1
강현구 <rkdgusrnrlrl@gmail.com>
2017-07-02 00:31:05 +0900

    fix photo list page

--- main/urls.py
+++ main/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^user/', include('allauth.urls')),
     url(r'^photos/', include('photos.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
