52a6c489bf50082fb27d3411b4357f09189f2114
강현구 <rkdgusrnrlrl@gmail.com>
2017-07-01 22:46:59 +0900

    add Photo Model

---
+++ main/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class Photo(models.Model):
+    author = models.ForeignKey('auth.User')
+    contents  = models.CharField(max_length=100)
+    created_date = models.DateField(default=timezone.now)
+    modify_date = models.DateField()
+    pic = models.ImageField()
+
+    def delete(self):
+        self.pic.delete()
+
+
+
--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'main',
 
     # allauth require option
     'allauth',
