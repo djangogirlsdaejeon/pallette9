fc478acd72b224b2c1ae7dcc1e7b7eb8652461dd
Paul Grau <paul@graycoding.com>
2017-07-02 04:05:10 +0900

    basic discovery feed and color picker

--- photos/DiscoveryPhotoList.py
+++ photos/DiscoveryPhotoList.py
@@ -5,11 +5,17 @@ from .models import Photo
 
 class DiscoveryPhotoList(ListView):
     model = Photo
+    paginate_by = 10
+    template_name = 'photos/feed-discovery.html'
 
-    paginate_by = 25
+    def get_context_data(self):
+        context = super(DiscoveryPhotoList, self).get_context_data()
+        context['available_colors'] = ('F44336', 'F4A1BE', '9575CD', '64B5F6', '4C4F51', '81C784', 'F5E25B', 'E3E7EA', 'FFB74D')
+        context['color'] = self.request.GET.get('color', '')
+        return context
 
     def get_queryset(self):
         color = self.request.GET.get('color', False)
         if color:
-            return Photo.objects.filter(color=color)
+            return Photo.objects.filter(color_tag=color)
         return Photo.objects.all()
--- photos/MyPhotoList.py
+++ photos/MyPhotoList.py
@@ -6,6 +6,7 @@ from .models import Photo
 class MyPhotoList(ListView):
     model = Photo
     paginate_by = 10
+    template_name = 'photos/feed-user.html'
 
     def get_queryset(self):
         return Photo.objects.filter(author=self.request.user)
